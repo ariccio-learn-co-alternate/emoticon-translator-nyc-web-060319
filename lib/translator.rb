cf70775948dc0e9fd9f8ca2438dba62f642b3c33
Alexander Riccio <test35965@gmail.com>
2019-05-31 19:58:24 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,25 @@
 # require modules here
+require "pry"
+require "yaml"
 
-def load_library
-  # code goes here
+def load_library(path)
+  emoticon_spec = YAML.load_file(path)
+  return_hash = {
+    "get_emoticon" => {},
+    "get_meaning" => {}
+  }
+  emoticon_spec.keys.each do |emoticon_meaning|
+    #binding.pry
+    return_hash["get_meaning"][emoticon_spec[emoticon_meaning][1]] = emoticon_meaning
+  end
+  #binding.pry
+  backwards_spec = emoticon_spec.invert
+  backwards_spec.keys.each do |emoticon_array|
+    return_hash["get_emoticon"][emoticon_array[1]] = backwards_spec[emoticon_array]
+  end
+  #binding.pry
+
+  return_hash
 end
 
 def get_japanese_emoticon
